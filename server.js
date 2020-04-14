5c7b7e863979b8225eb64b9c22c9fda3800d3f35
Wesley Osborne <wao218@lehigh.edu>
2020-04-14 00:11:13 -0400

    Set up all app routes

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.json({ msg: 'Welcome to the Contact Keeper API...'}));
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
