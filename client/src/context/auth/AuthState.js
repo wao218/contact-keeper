c9ac3d793cead3cdd3ef9485090d5b14aa9bc430
Wesley Osborne <wao218@lehigh.edu>
2020-04-24 15:14:18 -0400

    Connected User Registration form to backend

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -1,4 +1,5 @@
 import React, { useReducer } from 'react';
+import axios from 'axios';
 import AuthContext from './authContext';
 import authReducer from './authReducer';
 import {
@@ -26,14 +27,39 @@ const AuthState = (props) => {
   // ACTIONS
 
   // Load User
+  const loadUser = () => console.log('Load User');
 
   // Register User
+  const register = async (formData) => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    try {
+      const res = await axios.post('api/users', formData, config);
+
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: res.data
+      });
+    } catch (err) {
+      dispatch({
+        type: REGISTER_FAIL,
+        payload: err.response.data.msg
+      });
+    }
+  }
 
   // Login User
+  const login = () => console.log('login');
 
   // Logout
-
+  const logout = () => console.log('logout');
+  
   // Clear Errors
+  const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
 
 
   return (
@@ -43,7 +69,12 @@ const AuthState = (props) => {
         isAuthenticated: state.isAuthenticated,
         user: state.user,
         loading: state.loading,
-        error: state.error
+        error: state.error,
+        register,
+        loadUser,
+        login,
+        logout,
+        clearErrors
       }}
     >
       {props.children}
