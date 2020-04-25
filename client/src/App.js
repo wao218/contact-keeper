b6afb4b1185bd35d0259a60ea1b1c8c42545bd78
Wesley Osborne <wao218@lehigh.edu>
2020-04-24 21:26:33 -0400

    Added PrivateRoute component

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Home from './component/pages/Home';
 import About from './component/pages/About';
 import Register from './component/auth/Register';
 import Login from './component/auth/Login';
+import PrivateRoute from './component/routing/PrivateRoute';
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
 import AlertState from './context/alert/AlertState';
@@ -27,7 +28,7 @@ const App = () => {
               <div className="container">
                 <Alerts />
                 <Switch>
-                  <Route exact path='/' component={Home} />
+                  <PrivateRoute exact path='/' component={Home} />
                   <Route exact path='/about' component={About} />
                   <Route exact path='/register' component={Register} />
                   <Route exact path='/login' component={Login} />
