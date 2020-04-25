1a10562f469459924f38ca790e738175d08e469d
Wesley Osborne <wao218@lehigh.edu>
2020-04-24 22:34:14 -0400

    Connected update contact from frontend to backend

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -62,6 +62,21 @@ const ContactState = (props) => {
   
   }
 
+  // Update contact
+  const updateContact = async (contact) => {
+    const config = {
+      'Content-Type': 'application/json'
+    }
+
+    try {
+      const res = await axios.put(`/api/contacts/${contact._id}` , contact, config);
+      dispatch({ type: UPDATE_CONTACT, payload: res.data });
+    } catch (err) {
+      dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
+    }
+
+  }
+
   // Clear Contact
   const clearContacts = () => {
     dispatch({ type: CLEAR_CONTACTS });
@@ -75,11 +90,6 @@ const ContactState = (props) => {
   // Clear current contact
   const clearCurrent = () => {
     dispatch({ type: CLEAR_CURRENT });
-  }
-
-  // Update contact
-  const updateContact = (contact) => {
-    dispatch({ type: UPDATE_CONTACT, payload: contact });
   }
 
   // Filter contacts
